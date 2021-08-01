ab34886c7af144cd4557571bf77f96ee71b38aec
Tin Tran <tinhardware@gmail.com>
2021-08-01 20:31:34 +0700

    update information

--- _src/index.js
+++ _src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import Tobi from 'rqrauhvmra__tobi'
 const tobi = new Tobi()
 
+/*
 // Remove the two following lines to remove the product hunt floating prompt
 import FloatingPrompt from 'producthunt-floating-prompt'
 FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
@@ -14,3 +15,4 @@ function addDarkmodeWidget() {
   new Darkmode().showWidget()
 }
 window.addEventListener('load', addDarkmodeWidget)
+*/
